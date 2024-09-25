94daed22b8d2fe4cd3627e26d5c421a2f87fc74b
Maestro1982 <kris.tote@telenet.be>
2024-09-25 12:52:44 +0200

    finish pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
 
 const App = () => {
-    return (
-        <main className="overflow-clip">
-            <Header />
-        </main>
-    )
-}
-export default App
+  return (
+    <main className="overflow-clip">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+    </main>
+  );
+};
+export default App;
